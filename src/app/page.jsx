4f960f26f4a47cd83fa596440d6b8bdf4066ac65
Mayumino <uenomiyano@gmail.com>
2025-08-07 22:44:27 -0300

    07.08

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 export default function Home(){
   return(
-    <div>
+    <div className="container">
     <h1>Hello, World</h1>
-    <p>Primeiro exemplo no next</p>
-    <div className="quadrado"></div>
+    <p className="txtdestaque">Primeiro exemplo no next.</p> 
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptate quam distinctio dolor aperiam culpa saepe ducimus, molestias porro cum doloribus delectus tempore sunt aut. Architecto in non aut quo.</p>
+    <p className="txtpequeno">texto pequeno</p>
+    <div className="quadrado" ></div>
     </div>
   );
 }
